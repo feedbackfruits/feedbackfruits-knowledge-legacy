5934af4d81f229c42ac7cb4e32feb861055d0b13
Steffan Sluis <steffansluis@gmail.com>
2017-03-28 04:00:02 +0200

    Add autocomplete endpoint

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,8 @@ require('dotenv').load({ silent: true });
 export const {
   PORT = 4000,
   CAYLEY_ADDRESS = 'http://cayley:64210',
+  ELASTICSEARCH_ADDRESS = 'http://localhost:9200',
+  ELASTICSEARCH_INDEX_NAME = 'knowledge',
   MAG_API_ENDPOINT = 'https://academic.microsoft.com/api/browse/GetEntityDetails',
   DBPEDIA_ENDPOINT = 'http://dbpedia.org/resource/',
   DBPEDIA_SPARQL_ENDPOINT = 'http://dbpedia.org/sparql',
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import * as graphqlHTTP from 'express-graphql';
 
 import Schema from './schema';
 
+import Elasticsearch from './elasticsearch';
 
 export function create() {
   const server = express();
@@ -20,6 +21,22 @@ export function create() {
 
   // Disable favicon
   server.get('/favicon.ico', (req, res, next) => res.status(404).end());
+
+  server.get('/autocomplete', (req, res) => {
+    let { text } = req.query;
+    let query = `{
+      "size": 5,
+      "_source": "name",
+      "query": {
+        "match": {
+          "name": "${text}"
+        }
+      }
+    }`;
+    Elasticsearch(query).then(results => {
+      res.json(results).end();
+    }).catch(err => res.status(500).json(err).end());
+  });
 
   server.all('/', graphqlHTTP({
     schema: Schema,
