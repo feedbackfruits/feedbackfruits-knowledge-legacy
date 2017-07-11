10e28879404e8d5507ab4eceaac65f6914d33b3a
Steffan Sluis <steffansluis@gmail.com>
2017-07-11 20:55:28 +0200

    Seed Cayley data

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ require('dotenv').load({ silent: true });
 
 export const {
   PORT = 4000,
-  CAYLEY_ADDRESS = 'http://cayley:64210',
+  CAYLEY_ADDRESS = 'http://localhost:64210/',
   ELASTICSEARCH_ADDRESS = 'http://localhost:9200',
   ELASTICSEARCH_INDEX_NAME = 'knowledge',
   MAG_API_ENDPOINT = 'https://academic.microsoft.com/api/browse/GetEntityDetails',
